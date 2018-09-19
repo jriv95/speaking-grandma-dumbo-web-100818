7731f7747cece0ec689f5bf9208d05bda332f1d9
Joel Rivera <joel.rivera7995@gmail.com>
2018-09-19 04:13:50 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,11 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
 if string == "I LOVE YOU GRANDMA"
-  puts "I LOVE YOU TOO PUMPKIN"
+  print "I LOVE YOU TOO PUMPKIN"
 elsif string == string.upcase
-  puts "NO, NOT SINCE 1938!"
+  print "NO, NOT SINCE 1938!"
 else
-  puts "HUH?! SPEAK UP, SONNY!"
+  print "HUH?! SPEAK UP, SONNY!"
 end
 
 # Whatever you say to grandma, she should respond with
