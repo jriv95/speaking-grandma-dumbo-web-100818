29ac4f2ad019083439c146303510e6aada45139a
Joel Rivera <joel.rivera7995@gmail.com>
2018-09-19 04:05:34 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,5 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma(string)
-if string == string.upcase
-  puts "I LOVE YOU TOO PUMPKIN"
-elsif string == string.upcase
-  puts "NO, NOT SINCE 1938!"
-else
-  puts "HUH?! SPEAK UP, SONNY!"
-end
+
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
