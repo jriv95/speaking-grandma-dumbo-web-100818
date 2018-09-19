96df17993cd0cc7343a077ad0a31cb31d57e2d6e
Joel Rivera <joel.rivera7995@gmail.com>
2018-09-19 04:03:37 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma
-if speak_to_grandma == "I LOVE YOU GRANDMA"
+def speak_to_grandma(string)
+if string.upcase
   puts "I LOVE YOU TOO PUMPKIN"
-elsif speak_to_grandma == "WHAT DID YOU EAT TODAY?"
+elsif string.upcase
   puts "NO, NOT SINCE 1938!"
 else
   puts "HUH?! SPEAK UP, SONNY!"
@@ -12,8 +12,7 @@ end
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-#elsif speak_to_grandma == "Hi Nana, how are you?"
-  #print "HUH?! SPEAK UP, SONNY!"
+
 
 # If you shout, she can hear you (or at least she thinks so)
 # and yells back
