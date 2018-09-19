3ce289dc27824660cefef14eb74b1268c0424782
Joel Rivera <joel.rivera7995@gmail.com>
2018-09-19 04:05:01 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
-if string.upcase
+if string == string.upcase
   puts "I LOVE YOU TOO PUMPKIN"
-elsif string.upcase
+elsif string == string.upcase
   puts "NO, NOT SINCE 1938!"
 else
   puts "HUH?! SPEAK UP, SONNY!"
